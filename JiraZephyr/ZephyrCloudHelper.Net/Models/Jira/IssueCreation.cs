5735bc83b4d7edcb05d8feec616e4215cae56dcf
Michael ZHANG <yuyu.zh2012@gmail.com>
2019-04-29 14:56:29 +1000

    Remove customer field in issue creation

--- JiraZephyr/ZephyrCloudHelper.Net/Models/Jira/IssueCreation.cs
+++ JiraZephyr/ZephyrCloudHelper.Net/Models/Jira/IssueCreation.cs
@@ -22,14 +22,6 @@ namespace ZephyrCloudHelper.Net.Models.Jira
         public string name { get; set; }
     }
 
-    // This is the custom field for 'Automated' flag
-    // id = 10201 means Yes
-    // id = 10200 means No
-    public class Customfield10500
-    {
-        public string id { get; set; }
-    }
-
     public class Fields
     {
         public ProjectField project { get; set; }
@@ -37,7 +29,6 @@ namespace ZephyrCloudHelper.Net.Models.Jira
         public string description { get; set; }
         public Issuetype issuetype { get; set; }
         public Reporter reporter { get; set; }
-        public Customfield10500 customfield_10500 { get; set; }
         public List<string> labels { get; set; }
     }
 }
